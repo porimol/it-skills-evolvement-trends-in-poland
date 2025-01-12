2840782833e7798283dc9547f378a390ea90fd0b
Samidullo <127269426+samidulloabdullaev@users.noreply.github.com>
2025-01-12 21:45:36 +0100

    Update final_project.R

--- code/final_project.R
+++ code/final_project.R
@@ -50,3 +50,182 @@ str(df_sample)
 # memeroey usage of the data
 object.size(df_sample)
 
+
+# ploting part 
+
+df <- read_parquet("full_data.parquet")
+
+# cols to convert
+float_cols <- c('latitude', 'longitude')
+integer_cols <- c('b2b_from', 'b2b_to','permanent_from', 'permanent_to', 'mandate_from', 'mandate_to', 'skills_count')
+factor_cols <- c('experience_level', 'company_size_category', 'marker_icon', 'workplace_type', 'b2b_currency', 'permanent_currency', 'mandate_currency')
+
+# convert columns
+df[integer_cols] <- lapply(df[integer_cols], as.integer)
+df[float_cols] <- lapply(df[float_cols], as.numeric)
+df[factor_cols] <- lapply(df[factor_cols], as.factor)
+df$published_date <- as.Date(df$published_date)
+df$experience_level <- as.factor(df$experience_level)
+
+
+
+# Animated line plot grouped jobs by level - senior, middle and junior
+
+# Prepare the data for the animated line chart
+trend_data <- df %>%
+  group_by(published_date, experience_level) %>%
+  summarise(job_count = n(), .groups = "drop") %>%
+  arrange(published_date)
+
+# # Create the animated line chart
+# p <- ggplot(trend_data, aes(x = published_date, y = job_count, color = experience_level, group = experience_level)) +
+#   geom_line() +
+#   geom_point() +
+#   labs(title = 'Job Market Trends Over Time',
+#        x = 'Date',
+#        y = 'Number of Job Postings',
+#        color = 'Experience Level') +
+#   theme_minimal() +
+#   transition_reveal(published_date)
+
+# animate(p)
+
+# # save the animation
+# anim_save("job_market_trends.gif", animation = p)
+
+
+
+# create the animated line chart
+p1 <- ggplot(trend_data, aes(x = published_date, y = job_count, color = experience_level, group = experience_level)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 2) +
+  labs(title = 'Job Market Trends Over Time',
+       x = 'Date',
+       y = 'Number of Job Postings',
+       color = 'Experience Level') +
+  theme_minimal() +
+  theme(
+    plot.background = element_rect(fill = "lightblue", color = NA),
+    panel.background = element_rect(fill = "white", color = NA),
+    panel.grid.major = element_line(color = "gray80"),
+    panel.grid.minor = element_line(color = "gray90"),
+    plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
+    axis.title.x = element_text(size = 14, face = "bold"),
+    axis.title.y = element_text(size = 14, face = "bold"),
+    axis.text = element_text(size = 12),
+    axis.text.x = element_text(angle = 45, hjust = 1),  
+    legend.position = "top",
+    legend.title = element_text(size = 14, face = "bold"),
+    legend.text = element_text(size = 12)
+  ) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
+  scale_y_continuous(labels = scales::comma) +
+  transition_reveal(published_date)
+
+animate(p1)
+
+# save the animation
+anim_save("job_market_trends_1.gif", animation = p1)
+
+
+# plot 2 - Job Market Trends - line plot with marker icon trend each month
+
+df <- df %>%
+  mutate(year_month = floor_date(published_date, "month"))
+
+trend_data1 <- df %>%
+  group_by(year_month) %>%
+  summarise(job_count = n(), 
+            most_common_marker_icon = names(sort(table(marker_icon), decreasing = TRUE)[1:3]), 
+            .groups = 'drop') %>%
+  arrange(year_month)
+
+
+# Create the line chart without lines and using a different theme
+p <- ggplot(trend_data1, aes(x = year_month, y = job_count)) +
+  geom_line(linewidth = 1, color = "blue") +
+  geom_point(size = 3, color = "blue") +
+  geom_text_repel(aes(label = most_common_marker_icon), 
+                  nudge_y = 10, 
+                  arrow = arrow(length = unit(0.02, "npc")), 
+                  size = 5, 
+                  color = "red",
+                  max.overlaps = 20) +
+  labs(title = 'Number of Jobs Each Month',
+       x = '',
+       y = 'Number of Job Postings') +
+  theme_light() +  # Use a different theme
+  theme(
+    plot.background = element_rect(fill = "lightblue", color = NA),
+    panel.background = element_rect(fill = "#eadbdb", color = NA),
+    panel.grid.major = element_blank(),  # Remove major grid lines
+    panel.grid.minor = element_line(color = "#dad5d5"),  # Remove minor grid lines
+    plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
+    axis.title.x = element_text(size = 14, face = "bold"),
+    axis.title.y = element_text(size = 14, face = "bold"),
+    axis.text = element_text(size = 12),
+    axis.text.x = element_text(angle = 45, hjust = 1),  # Rotate x-axis labels
+    legend.position = "top",
+    legend.title = element_text(size = 14, face = "bold"),
+    legend.text = element_text(size = 12)
+  ) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
+  scale_y_continuous(labels = scales::comma)
+
+ggsave("job_market_trends_wide.png", plot = p, width = 14, height = 12)
+
+
+# Display the plot
+print(p)
+
+
+# plot 3 - salary analysis
+
+# Prepare the data for the box plot with jitter
+salary_data <- df %>%
+  filter(!is.na(b2b_from) & !is.na(b2b_to) & (b2b_currency == 'pln')) %>%
+  mutate(b2b_range = b2b_to - b2b_from)
+
+
+# Create the box plot with jitter
+p <- ggplot(salary_data, aes(x = experience_level, y = b2b_range, color = b2b_currency)) +
+  geom_boxplot(outlier.shape = NA, alpha = 0.5) +
+  geom_jitter(width = 0.2, size = 1.5, alpha = 0.7) +
+  labs(title = 'Salary Analysis by Experience Level and Currency',
+       x = 'Experience Level',
+       y = 'Salary Range (B2B)') +
+  theme_minimal() +
+  theme(
+    plot.background = element_rect(fill = "lightblue", color = NA),
+    panel.background = element_rect(fill = "white", color = NA),
+    panel.grid.major = element_line(color = "gray80"),
+    panel.grid.minor = element_line(color = "gray90"),
+    plot.title = element_text(size = 20, face = "bold", hjust = 0.5),
+    axis.title.x = element_text(size = 14, face = "bold"),
+    axis.title.y = element_text(size = 14, face = "bold"),
+    axis.text = element_text(size = 12),
+    legend.position = "top",
+    legend.title = element_text(size = 14, face = "bold"),
+    legend.text = element_text(size = 12)
+  ) +
+  scale_y_continuous(labels = scales::comma)
+
+# Display the plot
+print(p)
+
+
+# Prepare the data
+df_clean <- df %>%
+  filter(!is.na(b2b_from) & !is.na(b2b_to) & !is.na(experience_level))
+
+# Create the box plot with jitter
+ggplot(df_clean, aes(x = experience_level, y = b2b_from, color = b2b_currency)) +
+  geom_boxplot(outlier.colour = "red", outlier.size = 2, alpha = 0.5) +
+  geom_jitter(width = 0.1, alpha = 0.6) +
+  labs(title = "Salary Distribution by Experience Level and Currency",
+       x = "Experience Level", y = "Salary (From)") +
+  theme_minimal() +
+  theme(legend.position = "top") +
+  scale_color_manual(values = c("EUR" = "blue", "USD" = "green", "GBP" = "purple", "CHF" = "orange"))
+
+
